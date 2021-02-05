e909347680c9e96d710a8ede696b600eada5c6b4
SadayRED <sadayhann@gmail.com>
2021-02-06 02:53:19 +0300

    editprofile2

--- lib/pages/profil.dart
+++ lib/pages/profil.dart
@@ -11,33 +11,86 @@ class ProfilPageState extends State<ProfilPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            Container(
-              decoration: BoxDecoration(
+        body: SafeArea(
+      child: Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(
                 image: DecorationImage(
-                  image: NetworkImage(
-                      "https://c4.wallpaperflare.com/wallpaper/480/423/714/nissan-black-gtr34-wallpaper-preview.jpg"),
-                  fit: BoxFit.cover,
-                ),
-              ),
+                    image: NetworkImage(
+                        "https://c4.wallpaperflare.com/wallpaper/480/423/714/nissan-black-gtr34-wallpaper-preview.jpg"),
+                    fit: BoxFit.cover)),
+            child: Container(
+              width: double.infinity,
+              height: 200,
               child: Container(
-                width: double.infinity,
-                height: 200,
-                child: Container(
-                  alignment: Alignment(0.0, 2.5),
-                  child: CircleAvatar(
-                    backgroundImage:
-                        NetworkImage("https://i.hizliresim.com/QCpifz.png"),
-                    radius: 60.0,
-                  ),
+                alignment: Alignment(0.0, 2.5),
+                child: CircleAvatar(
+                  backgroundImage:
+                      NetworkImage("https://i.hizliresim.com/QCpifz.png"),
+                  radius: 60.0,
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+          SizedBox(
+            height: 60,
+          ),
+          Text(
+            "Oğuzhan SADAY",
+            style: TextStyle(
+                fontSize: 25.0,
+                color: Colors.blueGrey,
+                letterSpacing: 2.0,
+                fontWeight: FontWeight.w400),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            "İstanbul,Ataşehir",
+            style: TextStyle(
+                fontSize: 18.0,
+                color: Colors.black45,
+                letterSpacing: 2.0,
+                fontWeight: FontWeight.w300),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            "Işık Universitesi",
+            style: TextStyle(
+                fontSize: 15.0,
+                color: Colors.black45,
+                letterSpacing: 2.0,
+                fontWeight: FontWeight.w300),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Card(
+              margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 8.0),
+              elevation: 2.0,
+              child: Padding(
+                  padding: EdgeInsets.symmetric(vertical: 12, horizontal: 30),
+                  child: Text(
+                    "Öğrenci",
+                    style: TextStyle(
+                        letterSpacing: 2.0, fontWeight: FontWeight.w300),
+                  ))),
+          SizedBox(
+            height: 15,
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [],
+          )
+        ],
       ),
-    );
+    ));
   }
 }
