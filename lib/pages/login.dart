7e2af89ca7ecfc05f0040d121e86d25d16c4f4c1
Ahmetcan AKSU <ahmetcanco@gmail.com>
2021-02-06 22:56:28 +0300

    API Fix

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,8 +1,10 @@
 import 'dart:async';
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:gee/pages/home_page.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'register.dart';
+import 'package:http/http.dart' as http;
 import 'home_page.dart';
 
 class LoginPage extends StatefulWidget {
@@ -11,12 +13,11 @@ class LoginPage extends StatefulWidget {
 }
 
 class LoginPageState extends State<LoginPage> {
-  String statusText = " ";
+  final _formKey = GlobalKey<FormState>();
 
   TextEditingController kullaniciMailKontrolcusu = TextEditingController();
   TextEditingController kullaniciSifreKontrolcusu = TextEditingController();
 
-  get onPressed => null;
   @override
   void initState() {
     super.initState();
@@ -30,110 +31,118 @@ class LoginPageState extends State<LoginPage> {
         child: Center(
           child: Padding(
             padding: EdgeInsets.only(top: 20, right: 15, left: 15), //Sağdan, soldan ve üstden 10 pixel boşluk bırak
-            child: Column(
-              children: [
-                Text(
-                  "Hoşgeldin",
-                  style: TextStyle(fontSize: 36, fontFamily: "Montserrat"),
-                ),
-
-                Padding(
-                  child: Text(
-                    "Devam Etmek İçin Giriş Yap",
-                    style: TextStyle(fontSize: 14, fontFamily: "Montserrat"),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  Text(
+                    "Hoşgeldin",
+                    style: TextStyle(fontSize: 36, fontFamily: "Montserrat"),
                   ),
-                  padding: EdgeInsets.only(top: 22),
-                ),
-                Container(
-                  padding: EdgeInsets.only(top: MediaQuery.of(context).size.height / 6),
-                  margin: EdgeInsets.all(10),
-                  child: TextField(
-                    controller: kullaniciMailKontrolcusu, //Kullanıcının yazdığı şifreyi kontrol etmek için bunu kullanıyoruz
-                    decoration: InputDecoration(labelText: "Kullanıcı Mail"),
+                  Padding(
+                    child: Text(
+                      "Devam Etmek İçin Giriş Yap",
+                      style: TextStyle(fontSize: 14, fontFamily: "Montserrat"),
+                    ),
+                    padding: EdgeInsets.only(top: 22),
                   ),
-                ),
-                Container(
-                  margin: EdgeInsets.all(10),
-                  child: TextField(
-                    obscureText: true, //Yazı gizlensin mi
-                    obscuringCharacter: "*", //şifreyi gizleyecek karakter *****
-                    controller: kullaniciSifreKontrolcusu, //Kullanıcının yazdığı şifreyi kontrol etmek için bunu kullanıyoruz
-                    decoration: InputDecoration(labelText: "Kullanıcı Şifresi"),
-                  ),
-                ),
-
-                Text(statusText), //Kullanıcının Mail ve ya şifre hakkında bilgi aldığı element
-                TextButton(
-                  //Giriş Butonu
-
-                  onPressed: () async {
-                    if (kullaniciMailKontrolcusu.text.trim() == "") {
-                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                        content: Text("Mail Boş Olamaz!."),
-                      ));
-                    } else if (kullaniciSifreKontrolcusu.text.trim() == "") {
-                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                        content: Text("Sifre Boş Olamaz!."),
-                      ));
-                    } else {
-                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                        content: Text("Giriş Başarılı."),
-                      ));
-                    }
-                  },
-                  child: Text(""),
-                ),
-
-                ElevatedButton.icon(
-                  style: ElevatedButton.styleFrom(primary: Color.fromRGBO(158, 1, 49, 1)),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => HomePage(),
-                      ),
-                    );
-                  },
-                  icon: Icon(Icons.login, size: 12),
-                  label: Text("Giriş Yap", style: TextStyle(fontSize: 12.0, color: Colors.white)),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 10, bottom: 10),
-                  child: Text(
-                    "Veya",
-                    style: TextStyle(
-                      fontFamily: "Montserrat",
-                      color: Color.fromRGBO(100, 100, 100, 1),
+                  Container(
+                    padding: EdgeInsets.only(top: MediaQuery.of(context).size.height / 6),
+                    margin: EdgeInsets.all(10),
+                    child: TextFormField(
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Burası boş bırakılamaz';
+                        }
+                        return null;
+                      },
+                      controller: kullaniciMailKontrolcusu, //Kullanıcının yazdığı şifreyi kontrol etmek için bunu kullanıyoruz
+                      decoration: InputDecoration(labelText: "Kullanıcı Mail"),
                     ),
                   ),
-                ),
-                ElevatedButton.icon(
-                  style: ElevatedButton.styleFrom(primary: Color.fromRGBO(158, 1, 49, 1)),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => RegisterPage(),
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    padding: EdgeInsets.only(bottom: 20),
+                    child: TextFormField(
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Burası boş bırakılamaz';
+                        }
+                        return null;
+                      },
+                      obscureText: true, //Yazı gizlensin mi
+                      obscuringCharacter: "*", //şifreyi gizleyecek karakter *****
+                      controller: kullaniciSifreKontrolcusu, //Kullanıcının yazdığı şifreyi kontrol etmek için bunu kullanıyoruz
+                      decoration: InputDecoration(labelText: "Kullanıcı Şifresi"),
+                    ),
+                  ),
+                  ElevatedButton.icon(
+                    style: ElevatedButton.styleFrom(primary: Color.fromRGBO(158, 1, 49, 1)),
+                    onPressed: () async {
+                      if (_formKey.currentState.validate()) {
+                        final response = await http.post(
+                          'http://localhost/login',
+                          body: jsonEncode(
+                            <String, dynamic>{
+                              'mail': kullaniciMailKontrolcusu.text,
+                              'password': kullaniciSifreKontrolcusu.text,
+                            },
+                          ),
+                        );
+                        try {
+                          var data = jsonDecode(response.body);
+                          if (response.statusCode == 200) {
+                            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                              content: Text(data["code"].toString()),
+                            ));
+                          } else {
+                            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                              content: Text(data["code"].toString()),
+                            ));
+                          }
+                        } catch (_) {
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            content: Text("Sunucu hatası, bağlantı başarısız"),
+                          ));
+                        }
+                      }
+                      /*
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => HomePage(),
+                        ),
+                      );
+                      */
+                    },
+                    icon: Icon(Icons.login, size: 12),
+                    label: Text("Giriş Yap", style: TextStyle(fontSize: 12.0, color: Colors.white)),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 10, bottom: 10),
+                    child: Text(
+                      "Veya",
+                      style: TextStyle(
+                        fontFamily: "Montserrat",
+                        color: Color.fromRGBO(100, 100, 100, 1),
                       ),
-                    );
-                  },
-                  icon: Icon(Icons.app_registration, size: 12),
-                  label: Text("Kayıt ol", style: TextStyle(fontSize: 12.0, color: Colors.white)),
-                ),
-
-                /*onPressed: () {s
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => RegisterPage(),
-                      ),
-                    );
-                  },
-                  child: Text(
-                    "Kayıt Ol",
-                    style: TextStyle(fontSize: 12.0, color: Color.fromRGBO(158, 1, 49, 1)),
-                  ),*/
-              ],
+                    ),
+                  ),
+                  ElevatedButton.icon(
+                    style: ElevatedButton.styleFrom(primary: Color.fromRGBO(158, 1, 49, 1)),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => RegisterPage(),
+                        ),
+                      );
+                    },
+                    icon: Icon(Icons.app_registration, size: 12),
+                    label: Text("Kayıt ol", style: TextStyle(fontSize: 12.0, color: Colors.white)),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
--- lib/pages/register.dart
+++ lib/pages/register.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
 import 'package:gee/pages/login.dart';
 
 class RegisterPage extends StatefulWidget {
@@ -8,8 +11,11 @@ class RegisterPage extends StatefulWidget {
 }
 
 class RegisterPageState extends State<RegisterPage> {
-  String statusText = " ";
+  final _formKey = GlobalKey<FormState>();
+  bool onRequest = false;
 
+  TextEditingController kullaniciIsimKontrolcusu = TextEditingController();
+  TextEditingController kullaniciOkulKontrolcusu = TextEditingController();
   TextEditingController kullaniciMailKontrolcusu = TextEditingController();
   TextEditingController kullaniciSifreKontrolcusu = TextEditingController();
   TextEditingController kullaniciSifreDogrulayici = TextEditingController();
@@ -20,136 +26,166 @@ class RegisterPageState extends State<RegisterPage> {
     return Scaffold(
       body: Center(
         child: Padding(
-          padding: EdgeInsets.only(top: 10, right: 15, left: 15), //Sağdan, soldan ve üstden 10 pixel boşluk bırak
-          child: Column(
-            children: [
-              Text(
-                "Kayıt Ol",
-                style: TextStyle(fontSize: 36, fontFamily: "Montserrat"),
-              ),
-
-              Padding(
-                padding: EdgeInsets.only(top: 5),
-                child: Text(
-                  "Aramıza Katıl",
-                  style: TextStyle(fontSize: 14, fontFamily: "Montserrat"),
-                ),
-              ),
-
-              Container(
-                padding: EdgeInsets.only(
-                  top: 100,
-                ),
-                margin: EdgeInsets.all(10),
-                child: TextField(
-                  controller: kullaniciMailKontrolcusu, //Kullanıcının yazdığı şifreyi kontrol etmek için bunu kullanıyoruz
-                  decoration: InputDecoration(labelText: "Kullanıcı Maili"),
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.all(10),
-                child: TextField(
-                  obscureText: true, //Yazı gizlensin mi
-                  obscuringCharacter: "*", //şifreyi gizleyecek karakter *****
-                  controller: kullaniciSifreKontrolcusu, //Kullanıcının yazdığı şifreyi kontrol etmek için bunu kullanıyoruz
-                  decoration: InputDecoration(labelText: "Şifre Giriniz"),
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.all(10),
-                child: TextField(
-                  obscureText: true, //Yazı gizlensin mi
-                  obscuringCharacter: "*", //şifreyi gizleyecek karakter *****
-                  controller: kullaniciSifreDogrulayici, //Kullanıcının yazdığı şifreyi kontrol etmek için bunu kullanıyoruz
-                  decoration: InputDecoration(labelText: "Şifre Doğrulama"),
-                ),
-              ),
-
-              Padding(
-                padding: EdgeInsets.only(top: 20),
-                child: Text(statusText),
-              ), //Kullanıcının Mail ve ya şifre hakkında bilgi aldığı element
-              Padding(
-                padding: EdgeInsets.only(top: 10),
-                child: TextButton(
-                  //Giriş Butonu
-                  onPressed: () async {},
-                  child: ElevatedButton.icon(
-                    style: ElevatedButton.styleFrom(primary: Color.fromRGBO(158, 1, 49, 1)),
-                    onPressed: () {
-                      if (kullaniciMailKontrolcusu.text.trim() == "") {
-                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                          content: Text("Mail Boş Olamaz!"),
-                        ));
-                      } else if (kullaniciSifreDogrulayici.text.trim() == "") {
-                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                          content: Text("Şifre Boş Olamaz!"),
-                        ));
-                      } else if (kullaniciSifreDogrulayici.text.trim() != kullaniciSifreKontrolcusu.text.trim()) {
-                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                          content: Text("Şifreler Eşleşmedi!"),
-                        ));
-                      } else {
-                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                          content: Text("Giriş Başarılı"),
-                        ));
-                      }
-                    },
-                    icon: Icon(Icons.app_registration, size: 10),
-                    label: Text("Kayıt ol"),
+          padding: EdgeInsets.only(top: 10, right: 15, left: 15),
+          child: Form(
+            key: _formKey,
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Text(
+                    "Kayıt Ol",
+                    style: TextStyle(fontSize: 36, fontFamily: "Montserrat"),
                   ),
-                ),
-              ),
-              DropdownButton(
-                value: accountType,
-                icon: Icon(Icons.arrow_downward),
-                iconSize: 24,
-                elevation: 16,
-                style: TextStyle(color: Colors.deepPurple),
-                underline: Container(
-                  height: 2,
-                  color: Colors.deepPurpleAccent,
-                ),
-                onChanged: (int newValue) {
-                  setState(() {
-                    accountType = newValue;
-                  });
-                },
-                items: [0, 1].map<DropdownMenuItem<int>>((int value) {
-                  return DropdownMenuItem<int>(
-                    value: value,
-                    child: Text(value == 0 ? "Öğretmen" : "Öğrenci"),
-                  );
-                }).toList(),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 10, bottom: 10),
-                child: Text(
-                  "Veya",
-                  style: TextStyle(
-                    fontFamily: "Montserrat",
-                    color: Color.fromRGBO(100, 100, 100, 1),
+                  Padding(
+                    padding: EdgeInsets.only(top: 15),
+                    child: Text(
+                      "Aramıza Katıl",
+                      style: TextStyle(fontSize: 14, fontFamily: "Montserrat"),
+                    ),
                   ),
-                ),
+                  Container(
+                    padding: EdgeInsets.only(top: 50),
+                    margin: EdgeInsets.all(10),
+                    child: TextFormField(
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Burası boş bırakılamaz';
+                        }
+                        return null;
+                      },
+                      controller: kullaniciIsimKontrolcusu,
+                      decoration: InputDecoration(
+                        labelText: "İsim Soyisim",
+                      ),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(top: 10),
+                    margin: EdgeInsets.all(10),
+                    child: TextFormField(
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Burası boş bırakılamaz';
+                        }
+                        return null;
+                      },
+                      controller: kullaniciOkulKontrolcusu,
+                      decoration: InputDecoration(
+                        labelText: "Okul",
+                      ),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(
+                      top: 10,
+                    ),
+                    margin: EdgeInsets.all(10),
+                    child: TextField(
+                      controller: kullaniciMailKontrolcusu,
+                      decoration: InputDecoration(labelText: "Kullanıcı Maili"),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    child: TextFormField(
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Burası boş bırakılamaz';
+                        }
+                        return null;
+                      },
+                      obscureText: true,
+                      obscuringCharacter: "*",
+                      controller: kullaniciSifreKontrolcusu,
+                      decoration: InputDecoration(
+                        labelText: "Şifre Giriniz",
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    child: TextFormField(
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Burası boş bırakılamaz';
+                        }
+                        return null;
+                      },
+                      obscureText: true,
+                      obscuringCharacter: "*",
+                      controller: kullaniciSifreDogrulayici,
+                      decoration: InputDecoration(
+                        labelText: "Şifre Doğrulama",
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 20),
+                    child: TextButton(
+                      onPressed: () async {},
+                      child: ElevatedButton.icon(
+                        style: ElevatedButton.styleFrom(primary: Color.fromRGBO(158, 1, 49, 1)),
+                        onPressed: () async {
+                          if (_formKey.currentState.validate()) {
+                            final response = await http.post(
+                              'http://localhost/register',
+                              body: jsonEncode(
+                                <String, dynamic>{
+                                  'mail': kullaniciMailKontrolcusu.text,
+                                  'nameSurname': kullaniciIsimKontrolcusu.text,
+                                  'school': kullaniciOkulKontrolcusu.text,
+                                  'password': kullaniciSifreDogrulayici.text,
+                                  'accountType': accountType
+                                },
+                              ),
+                            );
+                            if (response.statusCode == 200) {
+                              print(jsonDecode(response.body));
+                            } else {
+                              print("");
+                            }
+                          }
+                        },
+                        icon: Icon(Icons.app_registration, size: 10),
+                        label: Text("Kayıt ol"),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 10, bottom: 10),
+                    child: Text(
+                      "Veya",
+                      style: TextStyle(
+                        fontFamily: "Montserrat",
+                        color: Color.fromRGBO(100, 100, 100, 1),
+                      ),
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: onRequest
+                        ? null
+                        : () {
+                            Navigator.of(context).pop();
+                          },
+                    child: ElevatedButton.icon(
+                      style: ElevatedButton.styleFrom(primary: Color.fromRGBO(158, 1, 49, 1)),
+                      onPressed: onRequest
+                          ? null
+                          : () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => LoginPage(),
+                                ),
+                              );
+                            },
+                      icon: Icon(Icons.backspace, size: 10),
+                      label: Text("Mevcut Hesaba Giriş Yap"),
+                    ),
+                  )
+                ],
               ),
-              TextButton(
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                  child: ElevatedButton.icon(
-                    style: ElevatedButton.styleFrom(primary: Color.fromRGBO(158, 1, 49, 1)),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => LoginPage(),
-                        ),
-                      );
-                    },
-                    icon: Icon(Icons.backspace, size: 10),
-                    label: Text("Mevcut Hesaba Giriş Yap"),
-                  ))
-            ],
+            ),
           ),
         ),
       ),
