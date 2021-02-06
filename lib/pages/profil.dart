8a0b2e309088917626c7842eeb1a6e9bc10c3c4f
SadayRED <sadayhann@gmail.com>
2021-02-06 03:46:00 +0300

    profilabout edit

--- lib/pages/profil.dart
+++ lib/pages/profil.dart
@@ -11,136 +11,162 @@ class ProfilPageState extends State<ProfilPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-      child: Column(
-        children: [
-          Container(
-            decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: NetworkImage(
-                        "https://c4.wallpaperflare.com/wallpaper/480/423/714/nissan-black-gtr34-wallpaper-preview.jpg"),
-                    fit: BoxFit.cover)),
-            child: Container(
-              width: double.infinity,
-              height: 200,
-              child: Container(
-                alignment: Alignment(0.0, 2.5),
-                child: CircleAvatar(
-                  backgroundImage:
-                      NetworkImage("https://i.hizliresim.com/QCpifz.png"),
-                  radius: 60.0,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: NetworkImage(
+                            "https://c4.wallpaperflare.com/wallpaper/480/423/714/nissan-black-gtr34-wallpaper-preview.jpg"),
+                        fit: BoxFit.cover)),
+                child: Container(
+                  width: double.infinity,
+                  height: 200,
+                  child: Container(
+                    alignment: Alignment(0.0, 2.5),
+                    child: CircleAvatar(
+                      backgroundImage:
+                          NetworkImage("https://i.hizliresim.com/QCpifz.png"),
+                      radius: 60.0,
+                    ),
+                  ),
                 ),
               ),
-            ),
-          ),
-          SizedBox(
-            height: 60,
-          ),
-          Text(
-            "Oğuzhan SADAY",
-            style: TextStyle(
-                fontSize: 25.0,
-                color: Colors.blueGrey,
-                letterSpacing: 2.0,
-                fontWeight: FontWeight.w400),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Text(
-            "İstanbul,Ataşehir",
-            style: TextStyle(
-                fontSize: 18.0,
-                color: Colors.black45,
-                letterSpacing: 2.0,
-                fontWeight: FontWeight.w300),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Text(
-            "Işık Universitesi",
-            style: TextStyle(
-                fontSize: 15.0,
-                color: Colors.black45,
-                letterSpacing: 2.0,
-                fontWeight: FontWeight.w300),
-          ),
-          SizedBox(
-            height: 6,
-          ),
-          Card(
-              margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 8.0),
-              elevation: 2.0,
-              child: Padding(
-                  padding: EdgeInsets.symmetric(vertical: 12, horizontal: 30),
-                  child: Text(
-                    "Öğrenci",
-                    style: TextStyle(
-                        letterSpacing: 2.0, fontWeight: FontWeight.w300),
-                  ))),
-          SizedBox(
-            height: 10,
-          ),
-          Card(
-            margin: EdgeInsets.symmetric(horizontal: 7.0, vertical: 8.0),
-            child: Padding(
-              padding: const EdgeInsets.all(4.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Expanded(
-                    child: Column(
-                      children: [
-                        Text(
-                          "Cevaplar",
-                          style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 14.0,
-                              fontWeight: FontWeight.w600),
-                        ),
-                        SizedBox(
-                          height: 7,
-                        ),
-                        Text(
-                          "15",
-                          style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 10.0,
-                              fontWeight: FontWeight.w300),
-                        )
-                      ],
-                    ),
-                  ),
-                  Expanded(
-                    child: Column(
-                      children: [
-                        Text(
-                          "Takipçi",
-                          style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 14.0,
-                              fontWeight: FontWeight.w600),
-                        ),
-                        SizedBox(
-                          height: 7,
-                        ),
-                        Text(
-                          "5",
-                          style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 10.0,
-                              fontWeight: FontWeight.w300),
-                        )
-                      ],
-                    ),
-                  ),
-                ],
+              SizedBox(
+                height: 60,
               ),
-            ),
+              Text(
+                "Oğuzhan SADAY",
+                style: TextStyle(
+                    fontSize: 25.0,
+                    color: Colors.blueGrey,
+                    letterSpacing: 2.0,
+                    fontWeight: FontWeight.w400),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "İstanbul,Ataşehir",
+                style: TextStyle(
+                    fontSize: 18.0,
+                    color: Colors.black45,
+                    letterSpacing: 2.0,
+                    fontWeight: FontWeight.w300),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "Işık Universitesi",
+                style: TextStyle(
+                    fontSize: 15.0,
+                    color: Colors.black45,
+                    letterSpacing: 2.0,
+                    fontWeight: FontWeight.w300),
+              ),
+              SizedBox(
+                height: 6,
+              ),
+              Card(
+                  margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 8.0),
+                  elevation: 2.0,
+                  child: Padding(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 12, horizontal: 30),
+                      child: Text(
+                        "Öğrenci",
+                        style: TextStyle(
+                            letterSpacing: 2.0, fontWeight: FontWeight.w300),
+                      ))),
+              SizedBox(
+                height: 10,
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(horizontal: 7.0, vertical: 8.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Expanded(
+                        child: Column(
+                          children: [
+                            Text(
+                              "Cevaplar",
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 14.0,
+                                  fontWeight: FontWeight.w600),
+                            ),
+                            SizedBox(
+                              height: 7,
+                            ),
+                            Text(
+                              "15",
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 10.0,
+                                  fontWeight: FontWeight.w300),
+                            )
+                          ],
+                        ),
+                      ),
+                      Expanded(
+                        child: Column(
+                          children: [
+                            Text(
+                              "Takipçi",
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 14.0,
+                                  fontWeight: FontWeight.w600),
+                            ),
+                            SizedBox(
+                              height: 7,
+                            ),
+                            Text(
+                              "5",
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 10.0,
+                                  fontWeight: FontWeight.w300),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                child: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: TextField(
+                    minLines: 10,
+                    maxLines: 15,
+                    autocorrect: false,
+                    decoration: InputDecoration(
+                        hintText: 'Bizlere Biraz Kendinden Bahset.',
+                        filled: true,
+                        fillColor: Color(0xFFDBEDFF),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                          borderSide: BorderSide(color: Colors.green),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                          borderSide: BorderSide(color: Colors.green),
+                        )),
+                  ),
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
-    ));
+    );
   }
 }
